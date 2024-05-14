3440c8748ee713142a5d98dbdc6c45baab2067c0
Johndaboy <145539556+Johndaboy@users.noreply.github.com>
2024-05-14 13:47:21 +0200

    .

--- install.lua
+++ install.lua
@@ -1,3 +1,3 @@
 wget https://raw.githubusercontent.com/Johndaboy/computercraft/main/chest_monitor.lua
 sleep(4)
-wget https://github.com/Johndaboy/computercraft/blob/main/startup.lua
+wget https://raw.githubusercontent.com/Johndaboy/computercraft/main/startup.lua
